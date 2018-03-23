77de375edf58f7b0ebf489bf74bde33f919b1051
huskyhoochu <dfg1499@gmail.com>
2018-03-23 17:57:29 +0900

    Create Header & Footer component 컴포넌트 스타일 가이드에 따른 작명 참고: http://vuejs.kr/jekyll/update/2017/03/13/vuejs-component-style-guide/

---
+++ src/components/structure/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-fluid">
+    <div class="top-line"></div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "app-header"
+  }
+</script>
+<style scoped>
+  .top-line {
+    height: 10px;
+    background-color: #FF9947;
+  }
+</style>
