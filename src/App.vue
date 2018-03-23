e31736ad771536ceaa0e9ff2af7054fd20c99867
huskyhoochu <dfg1499@gmail.com>
2018-03-23 17:58:37 +0900

    Add components: Header & Footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/structure/AppHeader'
+import AppFooter from './components/structure/AppFooter'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppHeader,
+    AppFooter
   }
 }
 </script>
@@ -21,8 +22,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #EBE8E2;
+}
+.container-fluid {
+  padding: 0;
 }
 </style>
