e9d57d87f820426b05afe11e5bd62beb735934a2
huskyhoochu <dfg1499@gmail.com>
2018-03-24 20:15:09 +0900

    Create Sidebar component

---
+++ src/components/structure/AppSidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    hi
+  </div>
+</template>
+<script>
+  export default {
+    name: "app-sidebar"
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/components/view/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        hello
+      </div>
+      <div class="col-md-4">
+        <app-sidebar/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import AppSidebar from '../structure/AppSidebar'
+  export default {
+    name: "home",
+    components: {
+      AppSidebar
+    }
+  }
+</script>
+<style scoped>
+</style>
