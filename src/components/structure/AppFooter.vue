c5f55d1bdfe3d46e7d5fdf2da41942a03d686b0c
huskyhoochu <dfg1499@gmail.com>
2018-03-24 21:33:30 +0900

    Modify v-for command

--- src/components/structure/AppFooter.vue
+++ src/components/structure/AppFooter.vue
@@ -9,22 +9,11 @@
           <div class="col-md-7 col-sm-7 m-3">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <p class="text-secondary mb-0">상호명: 서울마리아센터 | 대표: 김정순</p>
-              </li>
-              <li class="nav-item">
-                <p class="text-secondary mb-0">사업자등록번호: 105-86-86705 | 통신판매업신고번호: 서울중구-제9876호</p>
-              </li>
-              <li class="nav-item">
-                <p class="text-secondary mb-0">관광사업자등록번호: 국외 제2000-12호</p>
-              </li>
-              <li class="nav-item">
-                <p class="text-secondary mb-0">주소: 서울시 중구 명동길 80 가톨릭회관 326, 327호</p>
-              </li>
-              <li class="nav-item">
-                <p class="text-secondary mb-0">TEL: 02-723-5425 | FAX: 02-325-5645</p>
-              </li>
-              <li class="nav-item">
-                <p class="text-secondary mb-0">개인정보보호관리책임자: 김정순 (mariacenter@daum.net)</p>
+                <ul class="list-inline mb-0">
+                  <li v-for="(value, key) in informations" class="list-inline-item text-secondary mb-0">
+                    <strong>{{key}}:</strong> {{value}}
+                  </li>
+                </ul>
               </li>
             </ul>
           </div>
@@ -36,11 +25,26 @@
 </template>
 <script>
   export default {
-    name: "app-footer"
+    name: "app-footer",
+    data () {
+      return {
+        informations: {
+          상호명: '서울마리아센터',
+          대표: '김정순',
+          사업자등록번호: '105-86-86705',
+          통신판매업신고번호: '서울중구-제9876호',
+          관광사업자등록번호: '국외 제2000-12호',
+          개인정보보호관리책임자: '김정순 (mariacenter@daum.net)',
+          주소: '서울시 중구 명동길 80 가톨릭회관 326, 327호',
+          TEL: '02-723-5425',
+          FAX: '02-325-5645'
+        }
+      }
+    }
   }
 </script>
 <style scoped>
-  .nav-item > p {
+  .list-inline-item {
     font-size: 0.7rem;
   }
 </style>
