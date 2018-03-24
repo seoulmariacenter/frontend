6426200db45b0944459e01f6b8302792247cbb58
huskyhoochu <dfg1499@gmail.com>
2018-03-24 17:05:14 +0900

    Create Footer & Add grey logo

--- src/components/structure/AppFooter.vue
+++ src/components/structure/AppFooter.vue
@@ -1,5 +1,38 @@
 <template>
-
+  <div class="container-fluid">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-3 col-sm-3 ml-0 mt-3 mr-3 mb-3">
+            <img src="../../assets/logo_grey.svg" width="130" alt="logo">
+          </div>
+          <div class="col-md-7 col-sm-7 m-3">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <p class="text-secondary mb-0">상호명: 서울마리아센터 | 대표: 김정순</p>
+              </li>
+              <li class="nav-item">
+                <p class="text-secondary mb-0">사업자등록번호: 105-86-86705 | 통신판매업신고번호: 서울중구-제9876호</p>
+              </li>
+              <li class="nav-item">
+                <p class="text-secondary mb-0">관광사업자등록번호: 국외 제2000-12호</p>
+              </li>
+              <li class="nav-item">
+                <p class="text-secondary mb-0">주소: 서울시 중구 명동길 80 가톨릭회관 326, 327호</p>
+              </li>
+              <li class="nav-item">
+                <p class="text-secondary mb-0">TEL: 02-723-5425 | FAX: 02-325-5645</p>
+              </li>
+              <li class="nav-item">
+                <p class="text-secondary mb-0">개인정보보호관리책임자: 김정순 (mariacenter@daum.net)</p>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-2 col-sm-2"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   export default {
@@ -7,4 +40,7 @@
   }
 </script>
 <style scoped>
+  .nav-item > p {
+    font-size: 0.7rem;
+  }
 </style>
