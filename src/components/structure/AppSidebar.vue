d41c68e7e0737f6a955c5d1e648e7863509c036f
huskyhoochu <dfg1499@gmail.com>
2018-03-24 21:10:51 +0900

    Create sidebar card section 문의전화 & 계좌번호

--- src/components/structure/AppSidebar.vue
+++ src/components/structure/AppSidebar.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
-    hi
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><img class="mb-1" src="../../assets/svg/si-glyph-call-forward.svg" width="20" alt="phone"> 문의 전화:</h5>
+        <h2><strong>02-723-5425</strong></h2>
+        <hr>
+        <h5 class="card-title"><img class="mb-1" src="../../assets/svg/si-glyph-money-3.svg" width="20" alt="phone"> 계좌 번호:</h5>
+        <h6 class="card-subtitle text-secondary mt-1 mb-2"><strong>(우리은행)</strong> 서울마리아센터</h6>
+        <h2><strong>1005-682-025425</strong></h2>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,4 +18,10 @@
   }
 </script>
 <style scoped>
+  @media (min-width: 768px) and (max-width: 991px) {
+    .card-body > h2 {
+      font-size: 1.4rem;
+    }
+  }
+
 </style>
