986962d1ceec1564401379c2db43cbce3302060b
huskyhoochu <dfg1499@gmail.com>
2018-04-04 16:13:25 +0900

    Create dashboard & sidebar components

---
+++ src/components/contents/admin/AdminSidebar.vue
@@ -0,0 +1,29 @@
+<template>
+<nav class="col-md-3 bg-light sidebar">
+  <div class="sidebar-sticky m-3">
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <h2><strong>관리자 페이지</strong></h2>
+      </li>
+      <li class="nav-item">
+        <hr>
+      </li>
+      <li class="nav-item mb-2">
+        <h4>예약 관리</h4>
+      </li>
+      <li class="nav-item mb-2">
+        <h4>순례 상품 만들기</h4>
+      </li>
+      <li class="nav-item"></li>
+      <li class="nav-item"></li>
+    </ul>
+  </div>
+</nav>
+</template>
+<script>
+  export default {
+    name: "AdminSidebar"
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/components/contents/admin/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <admin-sidebar/>
+  </div>
+</template>
+<script>
+  import AdminSidebar from './AdminSidebar'
+  export default {
+    name: "Dashboard",
+    components: {
+      AdminSidebar
+    }
+  }
+</script>
+<style scoped>
+</style>
