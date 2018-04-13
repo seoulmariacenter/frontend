545726a54a9ccb39178208b84cada73d52c06e6e
huskyhoochu <dfg1499@gmail.com>
2018-04-13 20:33:54 +0900

    Delete unused methods

--- src/store/actions.js
+++ src/store/actions.js
@@ -30,29 +30,3 @@ export const signOut = ({commit}) => {
     name: 'Home'
   })
 };
-
-export const getScheduleListQuery = ({commit}, payload) => {
-  axios({
-    method: 'get',
-    url: this.$store.state.endpoints.baseUrl + this.$store.state.endpoints.travel + this.$route.params.pk + '/' +
-    this.$store.state.endpoints.date + parseInt(payload) + '/schedule/',
-    header: {
-      'Content-Type': 'application/json'
-    },
-    xsrfHeaderName: 'X-XSRF-TOKEN',
-    credentials: true
-  }).then((response) => {
-    commit('updateSchedule', {[payload]: response.data.results});
-  }).catch((error) => {
-    console.log(error)
-  })
-};
-
-export const iterateScheduleLoop = ({state, dispatch}) => {
-  let step;
-  console.log('hello');
-  console.log(state.dateCounts);
-  for (step = 1; step < state.dateCounts.last; step++) {
-    dispatch('getScheduleListQuery')
-  }
-};
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -20,9 +20,5 @@ export const removeInfo = () => {
 };
 
 export const updateDateCounts = (state, payload) => {
-  state.dateCounts = Array.from({length: payload}, (v, k) => k + 1)
-};
-
-export const updateSchedule = (state, payload) => {
-  state.scheduleInfo = payload
+  state.dateCounts = payload
 };
