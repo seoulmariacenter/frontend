0800d2a4dd9244e6d0004bc7fe418e12a2324a2f
huskyhoochu <dfg1499@gmail.com>
2018-04-13 21:47:37 +0900

    Rename components: ProductManagement

--- src/router/lazy.js
+++ src/router/lazy.js
@@ -6,6 +6,6 @@ export const SignIn = () => import('../components/contents/admin/SignIn.vue');
 
 export const Dashboard = () => import('../components/view/Dashboard.vue');
 
-export const Management = () => import('../components/contents/admin/ReservationManagement.vue');
+export const Management = () => import('../components/contents/admin/ProductManagement.vue');
 
 export const ProductDetail = () => import('../components/contents/admin/ProductDetail.vue');
