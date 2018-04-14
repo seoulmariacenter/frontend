d671c570900e8f692a1925fd5441fdc0c6b9ef8d
huskyhoochu <dfg1499@gmail.com>
2018-04-14 12:44:02 +0900

    Connect router-link to alert div 대시보드 최초 접속 시 나타나는 안내 문구를 직관적으로 클릭할 수 있도록 링크 적용

--- src/components/view/Dashboard.vue
+++ src/components/view/Dashboard.vue
@@ -3,14 +3,20 @@
     <admin-sidebar/>
     <router-view v-bind:property="content" v-on:manageContent="manageContent"/>
     <div class="col-md-8 bg-light" v-if="content">
-      <div class="m-3 row text-center">
-        <div class="col-sm alert alert-info m-2">
-            <h5>일정을 확인하거나 예약자를 보려면</h5>
-            <h4><strong>순례 상품 관리</strong></h4>
+      <div class="m-3 text-center align-middle">
+        <div class="d-block text-left">
+          <h2><strong>무엇을 하실 건가요?</strong></h2>
         </div>
-        <div class="col-sm alert alert-success m-2">
+        <hr>
+        <div class="m-2 d-flex justify-content-center">
+          <router-link :to="{name: 'Management'}" tag="div" class="alert alert-info m-2 hovering">
+            <h5>일정을 확인하고 예약자를 보려면</h5>
+            <h4><strong>순례 상품 관리</strong></h4>
+          </router-link>
+          <router-link :to="{name: 'Create'}" tag="div" class="alert alert-success m-2 hovering">
             <h5>새로운 순례 상품을 만들려면</h5>
             <h4><strong>순례 상품 만들기</strong></h4>
+          </router-link>
         </div>
       </div>
     </div>
@@ -38,4 +44,7 @@
   }
 </script>
 <style scoped>
+  .hovering:hover {
+    cursor: pointer;
+  }
 </style>
