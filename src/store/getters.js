d05f8d4198e566a4d19bd835fcce34f5baee30eb
huskyhoochu <dfg1499@gmail.com>
2018-04-15 17:16:19 +0900

    Refactor getNextDateText getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -18,11 +18,11 @@ export const getScheduleInfo = state => {
   return state.queryData.scheduleInfo
 };
 
-export const getNextDateText = state => {
+export const getNextDateText = (state, getters) => {
   if (state.queryData.dateTable.length === 0) {
     return '아직 일정이 없습니다'
   } else {
-    const dateText = state.queryData.dateTable[state.queryData.dateTable.length -1].date_time;
+    const dateText = getters.getDateTable[getters.getDateTable.length -1].date_time;
     const dateArray = dateText.split('-');
     const dateObj =  new Date(dateArray[0], parseInt(dateArray[1]), parseInt(dateArray[2]) + 1);
     return dateObj.getFullYear() + '-' + dateObj.getMonth() + '-' + dateObj.getDate()
