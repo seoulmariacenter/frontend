ede4af48266bbd3e3aa236bf594955c46ab9d81e
huskyhoochu <dfg1499@gmail.com>
2018-04-19 14:29:17 +0900

    Create ProductUpdateDestroy component

---
+++ src/components/contents/admin/product/ProductUpdateDestroy.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="m-3 ml-3">
+    <button class="btn btn-sm btn-outline-danger mr-1">순례 상품 수정</button>
+    <button type="button" class="btn btn-sm btn-danger ml-1" data-toggle="modal" data-target="#destroyModal">순례 상품 삭제</button>
+
+    <!--Modal-->
+    <div class="modal fade" id="destroyModal" tabindex="-1" role="dialog" aria-labelledby="destroyModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="destroyModalLabel">순례 상품 삭제</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            순례 상품을 정말 삭제하시겠습니까? 한번 삭제된 내용은 되돌릴 수 없습니다.
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">취소</button>
+            <button type="submit" class="btn btn-danger" @click="onSubmit"><strong>삭제하기</strong></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ProductUpdateDestroy",
+    methods: {
+      onSubmit() {
+
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
