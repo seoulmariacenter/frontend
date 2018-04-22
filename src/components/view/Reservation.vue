a5cd12bff5f7b6b7c7ac60fed7c9c596f12b3bc2
huskyhoochu <dfg1499@gmail.com>
2018-04-23 00:41:59 +0900

    Delete fetchData method

--- src/components/view/Reservation.vue
+++ src/components/view/Reservation.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="container">
-    <div class="loading" v-if="loading">
-      <h4>잠시만 기다려 주세요...</h4>
-    </div>
     <published-product-list/>
   </div>
 </template>
@@ -12,22 +9,6 @@
     name: "Reservation",
     components: {
       PublishedProductList
-    },
-    data() {
-      return {
-        loading: false
-      }
-    },
-    created() {
-      this.fetchData()
-    },
-    watch: {
-      '$route': 'fetchData'
-    },
-    methods: {
-      fetchData() {
-        this.$store.dispatch('getProductListQuery');
-      }
     }
   }
 </script>
