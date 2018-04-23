7a175535edbfdc26fc27d4d089d0df5c265f96b2
huskyhoochu <dfg1499@gmail.com>
2018-04-23 22:06:48 +0900

    Create reservation form

--- src/components/contents/reservation/ReservationForm.vue
+++ src/components/contents/reservation/ReservationForm.vue
@@ -2,63 +2,42 @@
   <div class="bg-light col-md-5 col-sm-12">
     <div class="m-3 pt-3 pb-3">
       <div class="d-block">
-        <h2><strong>예약 정보 입력</strong></h2>
+        <h2><strong>예약자 정보 입력</strong></h2>
       </div>
       <hr>
       <div class="row m-2">
-        <form action="">
-          <div class="form-group row">
-            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-            </div>
+        <form class="col-12" @submit.prevent="onSubmit">
+          <div class="form-row">
+            <label for="inputProduct" class="col-form-label">선택 상품</label>
+            <input type="text" class="form-control" id="inputProduct" placeholder="" disabled>
           </div>
-          <div class="form-group row">
-            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-            </div>
+          <div class="form-row">
+            <label for="inputName" class="col-form-label">이름</label>
+            <input v-model="name" type="text" class="form-control" id="inputName" placeholder="예약 인원의 대표자 성함을 입력하세요.">
           </div>
-          <fieldset class="form-group">
-            <div class="row">
-              <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-              <div class="col-sm-10">
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                  <label class="form-check-label" for="gridRadios1">
-                    First radio
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                  <label class="form-check-label" for="gridRadios2">
-                    Second radio
-                  </label>
-                </div>
-                <div class="form-check disabled">
-                  <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-                  <label class="form-check-label" for="gridRadios3">
-                    Third disabled radio
-                  </label>
-                </div>
+          <div class="form-row">
+            <label for="inputPhone" class="col-form-label">연락처</label>
+            <input v-model="phone" type="text" class="form-control" id="inputPhone" placeholder="핸드폰 번호를 입력하세요. 예) 010-1234-5678">
+          </div>
+          <fieldset class="form-row">
+            <legend class="col-form-label pb-0">성별</legend>
+            <div class="d-flex m-2">
+              <div class="form-check mr-4">
+                <input v-model="gender" class="form-check-input" type="radio" name="gridRadios" id="genderRadios1" value="True" checked>
+                <label class="form-check-label" for="genderRadios1">
+                  남성
+                </label>
               </div>
-            </div>
-          </fieldset>
-          <div class="form-group row">
-            <div class="col-sm-2">Checkbox</div>
-            <div class="col-sm-10">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck1">
-                <label class="form-check-label" for="gridCheck1">
-                  Example checkbox
+              <div class="form-check m1-4">
+                <input v-model="gender" class="form-check-input" type="radio" name="gridRadios" id="genderRadios2" value="False">
+                <label class="form-check-label" for="genderRadios2">
+                  여성
                 </label>
               </div>
             </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-sm-10">
-              <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
+          </fieldset>
+          <div class="form-group">
+            <button type="submit" class="btn btn-lg btn-info">예약하기!</button>
           </div>
         </form>
       </div>
@@ -67,7 +46,19 @@
 </template>
 <script>
   export default {
-    name: "ReservationForm"
+    name: "ReservationForm",
+    data() {
+      return {
+        name: '',
+        phone: '',
+        gender: 'True'
+      }
+    },
+    methods: {
+      onSubmit() {
+
+      }
+    }
   }
 </script>
 <style scoped>
