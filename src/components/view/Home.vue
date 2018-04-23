73e07cc8c399aa0164577db33a5c2dd113965a89
huskyhoochu <dfg1499@gmail.com>
2018-04-23 17:50:31 +0900

    Delete transition modules

--- src/components/view/Home.vue
+++ src/components/view/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container">
-    <transition name="fade">
-      <carousel v-show="show"/>
-    </transition>
+      <carousel/>
     <div class="row">
       <div class="col-md-8">
         <advertisement/>
@@ -23,30 +21,8 @@
       AppSidebar,
       Carousel,
       Advertisement
-    },
-    data() {
-      return {
-        show: false
-      }
-    },
-    created() {
-      this.fetchData()
-    },
-    watch: {
-      '$route': 'fetchData'
-    },
-    methods: {
-      fetchData() {
-        this.show = true
-      }
     }
   }
 </script>
 <style scoped>
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
 </style>
