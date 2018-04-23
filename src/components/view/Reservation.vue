982aa29e9fc5933457ef53a849199e5344e451f4
huskyhoochu <dfg1499@gmail.com>
2018-04-24 00:47:19 +0900

    Separate module of Reservation components - 예약 화면과 예약 완료 화면을 Reservation 컴포넌트에 같은 계층으로 종속시키기 위해 중간자인 ReservationManagement를 만들고 원래 컴포넌트들을 이쪽오로 옮김

--- src/components/view/Reservation.vue
+++ src/components/view/Reservation.vue
@@ -1,33 +1,11 @@
 <template>
   <div class="container">
-    <div class="row justify-content-between">
-      <reservation-form v-bind:productTitle="productTitle"/>
-      <published-product-list v-on:callProductInfo="callProductInfo"/>
-    </div>
+    <router-view/>
   </div>
 </template>
 <script>
-  import 'popper.js/dist/popper.min'
-  import PublishedProductList from '../contents/reservation/PublishedProductList'
-  import ReservationForm from  '../contents/reservation/ReservationForm'
   export default {
-    name: "Reservation",
-    components: {
-      PublishedProductList,
-      ReservationForm
-    },
-    data() {
-      return {
-        productPK: 0,
-        productTitle: ''
-      }
-    },
-    methods: {
-      callProductInfo(payload) {
-        this.productPK = payload[0];
-        this.productTitle = payload[1]
-      }
-    }
+    name: "Reservation"
   }
 </script>
 <style scoped>
