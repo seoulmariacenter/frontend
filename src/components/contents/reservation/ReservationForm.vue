970ddf7c1aaed3b5edb0e627fd6640efd103c734
huskyhoochu <dfg1499@gmail.com>
2018-04-24 19:32:07 +0900

    Add validation rules

--- src/components/contents/reservation/ReservationForm.vue
+++ src/components/contents/reservation/ReservationForm.vue
@@ -10,17 +10,18 @@
           <div class="form-row">
             <label for="inputProduct" class="col-form-label">선택 상품</label>
             <input type="text" class="form-control" id="inputProduct" :placeholder="productTitle" disabled>
+            <small v-if="!productTitle" class="text-danger">우측의 순례 상품을 반드시 선택해주세요!</small>
           </div>
           <div class="form-row">
-            <label for="inputName" class="col-form-label">이름</label>
-            <input v-model="name" type="text" class="form-control" id="inputName" placeholder="예약 인원의 대표자 성함을 입력하세요." required>
+            <label for="inputName" class="col-form-label">예약자 성함</label>
+            <input v-model="name" type="text" class="form-control" id="inputName" placeholder="전체 예약 인원의 대표자 성함을 입력하세요." required>
           </div>
           <div class="form-row">
             <label for="inputPhone" class="col-form-label">연락처</label>
             <input v-model="phone" type="text" class="form-control" id="inputPhone" placeholder="핸드폰 번호를 입력하세요. 예) 010-1234-5678" required>
           </div>
           <fieldset class="form-row">
-            <legend class="col-form-label pb-0">성별</legend>
+            <legend class="col-form-label pb-0">예약자 성별</legend>
             <div class="d-flex m-2">
               <div class="form-check mr-4">
                 <input v-model="gender" class="form-check-input" type="radio" name="gridRadios" id="genderRadios1" value="True" checked>
