5efb409bcf74339b744a6e01b347f27462e93507
huskyhoochu <dfg1499@gmail.com>
2018-04-25 17:09:59 +0900

    Create reservation detail & update components

--- src/components/contents/reservation/ReservationDetail.vue
+++ src/components/contents/reservation/ReservationDetail.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="">
-    hello
-    {{getReservationInfo}}
+  <div class="row justify-content-between">
+    <reservation-update/>
+    <div class="bg-light col-md-5 col-sm-12">
+      <div class="m-3 pt-3 pb-3">
+        <div class="d-block d-flex justify-content-between">
+          <h2><strong>주의사항</strong></h2>
+          <p class="mt-3 mb-0">반드시 주의 깊게 읽어주세요!</p>
+        </div>
+        <hr>
+        <div class="row m-0 justify-content-center overflow">
+          <div class="card text-white bg-danger">
+            <div class="card-header">예약 관련</div>
+            <div class="card-body">
+              <h5 class="card-title">중도 해지</h5>
+              <p class="card-text">{{cautionText.termination}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-  import {mapGetters} from 'vuex'
+  import ReservationUpdate from './ReservationUpdate'
   export default {
     name: "ReservationDetail",
-    computed: {
-      ...mapGetters([
-        'getReservationInfo'
-      ])
+    components: {
+      ReservationUpdate
+    },
+    data() {
+      return {
+        cautionText: {
+          termination:
+          '서울마리아센터는 여행 경비를 절감하기 위해 최소 인원이 갖춰지는 대로 항공권을 단체 예매하고 있습니다. ' +
+          '만일 순례 직전에 예약을 취소하시면 항공권 취소에 따른 위약금이 발생할 수 있으니 참고 바랍니다.'
+        }
+      }
     }
   }
 </script>
 <style scoped>
+  .h330 {
+    height: 330px;
+  }
+  .overflow {
+    overflow: scroll;
+  }
 </style>
---
+++ src/components/contents/reservation/ReservationUpdate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-light col-md-6 col-sm-12">
+    <div class="m-3 pt-3 pb-3">
+      <div class="d-block d-flex justify-content-between">
+        <h2><strong>예약 정보</strong></h2>
+      </div>
+      <hr>
+      <div class="row m-2">
+        <div class="col p-0 card">
+          <div class="card-header">
+            <h5 class="mb-0"><strong>예약 상품:</strong></h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">예약자 성함: {{getReservationInfo.username}}</li>
+            <li class="list-group-item">예약자 연락처: {{getReservationInfo.phone_number}}</li>
+            <li class="list-group-item">예약자 성별:
+              <span v-if="getReservationInfo.gender">여성</span>
+              <span v-else>남성</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import {mapGetters} from 'vuex'
+  export default {
+    name: "ReservationUpdate",
+    computed: {
+      ...mapGetters([
+        'getReservationInfo'
+      ])
+    }
+  }
+</script>
+<style scoped>
+</style>
