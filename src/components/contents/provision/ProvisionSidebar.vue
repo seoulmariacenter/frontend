3a54f96bb21d46e37034e3301af413a7778d41b3
huskyhoochu <dfg1499@gmail.com>
2018-04-27 20:10:23 +0900

    Create Provision component

---
+++ src/components/contents/provision/ProvisionSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-lg-3 col-md-3 bg-light sidebar">
+    <div class="sidebar-sticky m-3">
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><h4><strong>서울마리아센터 소개</strong></h4></li>
+        <li class="nav-item mb-2">대표 인사말</li>
+        <li class="nav-item mb-2">성모님의 부르심을 찾아서</li>
+        <li class="nav-item mb-2">여행 약관</li>
+        <li class="nav-item mb-2">서비스 이용 약관</li>
+        <li class="nav-item mb-2">개인정보 처리방침</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ProvisionSidebar"
+  }
+</script>
+<style scoped>
+</style>
