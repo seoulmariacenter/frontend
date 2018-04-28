09399d440b667853e4e45bd3077ff5928b66162e
huskyhoochu <dfg1499@gmail.com>
2018-04-28 19:49:06 +0900

    Implement privacy policy provision

--- src/components/contents/provision/ProvisionPrivate.vue
+++ src/components/contents/provision/ProvisionPrivate.vue
@@ -5,12 +5,22 @@
         <h2><strong>개인정보 처리방침</strong></h2>
       </div>
       <hr>
+      <div class="m-3">
+        <div class="card border-secondary h750 overflow">
+          <div class="card-body" v-html="privatePolicy"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
   export default {
-    name: "ProvisionPrivate"
+    name: "ProvisionPrivate",
+    computed: {
+      privatePolicy() {
+        return require('../../../assets/markdown/privacy_policy.md')
+      }
+    }
   }
 </script>
 <style scoped>
