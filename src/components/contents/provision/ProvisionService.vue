7761de1ac626f99bef7b7280c9938943d1ebf12b
huskyhoochu <dfg1499@gmail.com>
2018-04-28 19:19:23 +0900

    Add & Implement cyber mall provision

--- src/components/contents/provision/ProvisionService.vue
+++ src/components/contents/provision/ProvisionService.vue
@@ -5,13 +5,30 @@
         <h2><strong>서비스 이용 약관</strong></h2>
       </div>
       <hr>
+      <div class="m-3">
+          <div class="card border-secondary h750 overflow">
+            <div class="card-body" v-html="cyberMallProvision"></div>
+          </div>
+      </div>
+      <hr>
     </div>
   </div>
 </template>
 <script>
   export default {
-    name: "ProvisionService"
+    name: "ProvisionService",
+    computed: {
+      cyberMallProvision() {
+        return require('../../../assets/markdown/cyber_mall_provision.md')
+      }
+    }
   }
 </script>
 <style scoped>
+  .h750 {
+    height: 750px;
+  }
+  .overflow {
+    overflow: scroll;
+  }
 </style>
