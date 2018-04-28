89c78aef06d279179b981ca1d0cd69c71408bdeb
huskyhoochu <dfg1499@gmail.com>
2018-04-28 20:51:40 +0900

    Rename title

--- src/components/contents/provision/ProvisionMaria.vue
+++ src/components/contents/provision/ProvisionMaria.vue
@@ -2,7 +2,7 @@
   <div class="col-lg-8 col-md-8 bg-light">
     <div class="m-3">
       <div class="d-block">
-        <h2><strong>성모님의 발자취</strong></h2>
+        <h2><strong>성모님의 부르심을 찾아서</strong></h2>
       </div>
       <hr>
     </div>
