02802aba7127b6fc3326b95c48acab9e7d93ad05
huskyhoochu <dfg1499@gmail.com>
2018-05-01 21:53:53 +0900

    Rename variable & Create close button

--- src/components/contents/admin/reservation/ReservationList.vue
+++ src/components/contents/admin/reservation/ReservationList.vue
@@ -9,9 +9,9 @@
               예약자 정렬
             </button>
             <div class="dropdown-menu">
-              <button @click="canceledQuery = 'all'" type="button" class="dropdown-item">전체 명단</button>
-              <button @click="canceledQuery = 'complete'" type="button" class="dropdown-item">예약 완료 명단</button>
-              <button @click="canceledQuery = 'canceled'" type="button" class="dropdown-item">취소자 명단</button>
+              <button @click="reservationQuery = 'all'" type="button" class="dropdown-item">전체 명단</button>
+              <button @click="reservationQuery = 'complete'" type="button" class="dropdown-item">예약 완료 명단</button>
+              <button @click="reservationQuery = 'canceled'" type="button" class="dropdown-item">취소자 명단</button>
             </div>
           </div>
           <p class="mt-3 mb-3">총 <span class="text-info">{{getActiveReservationCount}}</span> 명 예약 중</p>
@@ -21,7 +21,12 @@
       <div class="row m-2" v-if="isCanceledQuery">
         <div class="col-md-6 col-sm-12 mb-3" v-for="value in isCanceledQuery" :key="value.id">
           <div class="card">
-            <h5 class="card-header">예약자: {{value.username}}</h5>
+            <div class="card-header">
+              <h5 class="d-inline-block">예약자: {{value.username}}</h5>
+              <button @click="showModal = true" type="button" class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
             <div class="card-body">
               <h5 class="card-title">연락처: {{value.phone_number}}</h5>
               <p class="card-text">예약 일시: {{new Date(value.date_joined).getFullYear() + '-' + new Date(value.date_joined).getMonth() + '-' + new Date(value.date_joined).getDate()}}</p>
@@ -54,7 +59,7 @@
           true: '완료',
           false: '취소함'
         },
-        canceledQuery: 'all',
+        reservationQuery: 'all'
       }
     },
     created() {
@@ -80,11 +85,11 @@
         'getActiveReservationCount'
       ]),
       isCanceledQuery() {
-        if (this.canceledQuery === 'all') {
+        if (this.reservationQuery === 'all') {
          return this.getReservationQuery
-        } else if (this.canceledQuery === 'complete') {
+        } else if (this.reservationQuery === 'complete') {
           return this.getActiveReservationQuery
-        } else if (this.canceledQuery === 'canceled') {
+        } else if (this.reservationQuery === 'canceled') {
           return this.getInactiveReservationQuery
         }
       }
