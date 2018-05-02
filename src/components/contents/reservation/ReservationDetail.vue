a676adbb8efa3acc1abc060ba296e46d4103c65b
huskyhoochu <dfg1499@gmail.com>
2018-05-02 19:42:38 +0900

    Move caution card

--- src/components/contents/reservation/ReservationDetail.vue
+++ src/components/contents/reservation/ReservationDetail.vue
@@ -4,26 +4,6 @@
     <div class="bg-light col-md-5 col-sm-12">
       <div class="m-3 pt-3 pb-3">
         <div class="d-block d-flex justify-content-between">
-          <h2><strong>주의사항</strong></h2>
-          <p class="mt-3 mb-0">반드시 주의 깊게 읽어주세요!</p>
-        </div>
-        <hr>
-        <div class="card text-white bg-danger">
-          <div class="card-header"><h4 class="mb-0"><strong>예약 취소에 관하여</strong></h4></div>
-          <div class="card-body">
-            <p class="card-text mb-1">
-              서울마리아센터는 여행 경비를 절감하기 위해 최소 인원이 갖춰지는 대로 항공권을 단체 예매하고 있습니다.
-            </p>
-            <p class="card-text">
-              만일 여행 일정이 임박했을 때 예약을 취소하시면
-              <router-link :to="{name: 'Compensation'}" class="text-dark" target="_blank"><strong>소비자 분쟁 해결 기준</strong></router-link>
-              에 따른 손해배상액을 부담하실 수 있으니 참고 바랍니다.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="m-3 pt-3 pb-3">
-        <div class="d-block d-flex justify-content-between">
           <h2><strong>동승자 목록</strong></h2>
           <p class="mt-3 mb-0">동승자 인원: 총 <span class="text-info">{{getReservationMemberCount}}</span> 명</p>
         </div>
@@ -58,9 +38,11 @@
               </div>
             </div>
             <div class="card-body">
-              <p class="mb-1">연락처: {{index.phone_number}}</p>
-              <p class="mb-0" v-if="index.gender">성별: 여성</p>
-              <p class="mb-0" v-else>성별: 남성</p>
+              <p class="mb-0"><strong>연락처:</strong> {{index.phone_number}} |
+                <span class="mb-0" v-if="index.gender"><strong>성별:</strong> 여성</span>
+                <span class="mb-0" v-else><strong>성별:</strong> 남성</span>
+              </p>
+
             </div>
           </div>
         </div>
--- src/components/contents/reservation/ReservationInformaion.vue
+++ src/components/contents/reservation/ReservationInformaion.vue
@@ -39,6 +39,20 @@
               </div>
               <form>
                 <div class="modal-body">
+                  <div class="card text-white bg-danger mb-4">
+                    <div class="card-header"><h4 class="mb-0"><strong>예약 취소에 관하여</strong></h4></div>
+                    <div class="card-body">
+                      <p class="card-text mb-1">
+                        서울마리아센터는 여행 경비를 절감하기 위해 최소 인원이 갖춰지는 대로 항공권을 단체 예매하고 있습니다.
+                      </p>
+                      <p class="card-text">
+                        만일 여행 일정이 임박했을 때 예약을 취소하시면
+                        <router-link :to="{name: 'Compensation'}" class="text-dark" target="_blank">
+                          <strong>소비자 분쟁 해결 기준</strong></router-link>
+                        에 따른 손해배상액을 부담하실 수 있으니 참고 바랍니다.
+                      </p>
+                    </div>
+                  </div>
                   <p>정말로 예약을 취소하시겠습니까?</p>
                   <p>여행 일정이 임박했을 때 예약을 취소하시면 <strong>손해배상액을 부담</strong>하셔야 할 수도 있습니다.</p>
                   <p>본인 확인을 위해 예약번호 16자리를 다시 한번 입력해주십시오.</p>
