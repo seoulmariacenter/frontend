2cf898c9afefcaf25bfd1fe1fc4ab4120dd2eb6a
huskyhoochu <dfg1499@gmail.com>
2018-05-03 08:08:22 +0900

    Separate client-product components

---
+++ src/components/contents/clientproduct/ClientProductDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-light">
+    <div class="p-3">
+      <div class="d-block">
+        <h2><strong>순례 상품 보기</strong></h2>
+      </div>
+      <hr>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "ClientProductDetail"
+  }
+</script>
+<style scoped>
+</style>
