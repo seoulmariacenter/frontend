6773a01a78192be36e2bab4c09d59d820ac5afc4
huskyhoochu <dfg1499@gmail.com>
2018-05-04 10:54:02 +0900

    Modify CSS of dashboard

--- src/components/view/Dashboard.vue
+++ src/components/view/Dashboard.vue
@@ -8,15 +8,27 @@
           <h2><strong>무엇을 하실 건가요?</strong></h2>
         </div>
         <hr>
-        <div class="m-2 justify-content-center">
-          <router-link :to="{name: 'ProductManagement'}" tag="div" class="alert alert-info m-4 hovering">
-            <h5>일정을 확인하고 예약자를 보려면</h5>
-            <h4><strong>순례 상품 관리</strong></h4>
-          </router-link>
-          <router-link :to="{name: 'ProductCreate'}" tag="div" class="alert alert-success m-4 hovering">
-            <h5>새로운 순례 상품을 만들려면</h5>
-            <h4><strong>순례 상품 만들기</strong></h4>
-          </router-link>
+        <div class="row">
+          <div class="col-md-6 col-sm-12 mb-4">
+            <div class="card border-secondary text-center">
+              <h4 class="card-header text-secondary"><strong>일정을 확인하고 예약자를 보려면</strong></h4>
+              <div class="card-body">
+                <router-link :to="{name: 'ProductManagement'}" tag="button" class="btn btn-outline-secondary btn-lg">
+                  <h4 class="mb-0"><strong>순례 상품 관리</strong></h4>
+                </router-link>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-12 mb-4">
+            <div class="card border-info text-center">
+              <h4 class="card-header text-info"><strong>새로운 순례 상품을 만들려면</strong></h4>
+              <div class="card-body">
+                <router-link :to="{name: 'ProductCreate'}" tag="button" class="btn btn-outline-info btn-lg">
+                  <h4 class="mb-0"><strong>순례 상품 만들기</strong></h4>
+                </router-link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
