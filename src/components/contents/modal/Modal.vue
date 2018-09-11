6577365b159f5b31942610384191fd106ad9497d
huskyhoochu <dfg1499@gmail.com>
2018-09-11 19:12:07 +0900

    [#2] 파티마 성지순례단 모달 생성

---
+++ src/components/contents/modal/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal fade" id="fatima-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">파티마 101주년 성지순례단 모집</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <img src="../../../assets/image/modal/modal.jpeg" class="img-fluid" alt="fatima">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import $ from 'jquery'
+  export default {
+    name: "Modal",
+    mounted() {
+      $('#fatima-modal').modal('toggle');
+    }
+  }
+</script>
+<style scoped>
+
+</style>
--- src/components/view/Home.vue
+++ src/components/view/Home.vue
@@ -9,18 +9,21 @@
         <app-sidebar/>
       </div>
     </div>
+    <Modal/>
   </section>
 </template>
 <script>
   import AppSidebar from '../structure/AppSidebar'
   import MainProductView from '../contents/home/MainProductView'
   import Carousel from '../contents/home/Carousel'
+  import Modal from '../contents/modal/Modal'
   export default {
     name: "home",
     components: {
       AppSidebar,
       MainProductView,
-      Carousel
+      Carousel,
+      Modal,
     }
   }
 </script>
